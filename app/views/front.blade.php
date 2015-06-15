b4f78bdec6530cc2fc000b5d219cce5d000b8562
webvolant <barkalov_anton@mail.ru>
2015-06-15 11:33:12 +0200

    full images2

--- app/views/front.blade.php
+++ app/views/front.blade.php
@@ -20,52 +20,32 @@
         <meta name="description" content="@yield('description', Helper::description())">
     @show
 
-        <!-- CSS are placed here -->
         {{ HTML::style('css/bootstrap.css') }}
         {{ HTML::style('css/bootstrap-theme.css') }}
         {{ HTML::style('template.css') }}
 
         {{ HTML::style('source/jquery.fancybox.css') }}
+        {{ HTML::style('css/component.css') }}
+        {{ HTML::style('bootstrap-multiselect-master/dist/css/bootstrap-multiselect.css') }}
+        {{ HTML::style('slick/slick.css') }}
+        {{ HTML::style('slick/slick-theme.css') }}
+        {{ HTML::style('css/sweet-alert.css') }}
 
-    {{ HTML::style('css/component.css') }}
 
-        <!-- Scripts are placed here -->
         {{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js') }}
         {{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.3.1/jquery.maskedinput.js') }}
-        {{ HTML::script('js/bootstrap.js') }}
-
+        {{ HTML::script('js/bootstrap.min.js') }}
         {{ HTML::script('js/jquery.chained.min.js') }}
-
-        {{ HTML::style('bootstrap-multiselect-master/dist/css/bootstrap-multiselect.css') }}
-
-        {{ HTML::script('js/jquery.simpletip-1.3.1.min.js') }}
-
         {{ HTML::script('js/jquery.mousewheel-3.0.6.pack.js') }}
         {{ HTML::script('source/jquery.fancybox.pack.js') }}
-
         {{ HTML::script('js/sweet-alert.min.js') }}
-
-            {{ HTML::style('slick/slick.css') }}
-            {{ HTML::style('slick/slick-theme.css') }}
-
-            {{ HTML::script('slick/slick.js') }}
-
-    {{ HTML::script('js/modernizr.custom.js') }}
+        {{ HTML::script('slick/slick.min.js') }}
+        {{ HTML::script('js/modernizr.custom.js') }}
 
 
-        {{ HTML::style('css/sweet-alert.css') }}
         <script>
             $(document).ready(function() {
                 $("#krit2").chained("#krit1");
-                $(".statistics").simpletip({
-
-                    // Configuration properties
-                    content: 'Simple',
-                    fixed: true
-
-                });
-                //$('#basicModal').modal();
-                //$('#basicModal').modal();
 
                 $('.car_grafik').slick({
                     infinite: true,
@@ -83,7 +63,6 @@
 
             });
         </script>
-
 
 
     @show
@@ -131,19 +110,12 @@
 
                     @section('search')
 
-                <!--<div class="col-xs-12 col-sm-12 col-md-3">
-
-                    <div class="form-group">
-                        <h5 class="blau_text"></h5>
-                            </div>
-                        </div>-->
-
                         <div class="col-xs-4 col-sm-5 col-md-5 padding_left_right">
                             {{ Form::open(array('url' => 'search', 'role' => 'form', 'class' => '')) }}
                                 <div class="form-group">
 
                                     <select id="krit1" name="krit1" class="form-control">
-                                        <option value="">Искать</option>
+                                        <option value="">Критерий поиска</option>
                                         @foreach ($search1 as $key => $item)
                                         <option value="{{ $key }}">{{ $item }}</option>
                                         @endforeach
@@ -156,7 +128,7 @@
                             <div class="form-group">
                                 <a id="test_icon" class="pull-left" data-toggle="modal" data-target="#testsModal" class="btn btn-info btn-sm" ><span class="glyphicon glyphicon-edit"></span></a>
                                 <select id="krit2" name="krit2" class="form-control">
-                                    <option value="">Теперь параметр</option>
+                                    <option value="">Выберите параметр</option>
 
                                     @foreach ($specialities as $key => $item)
                                     <option value="{{ $key }}" class="0">{{ $item }}</option>
@@ -383,12 +355,8 @@
 
 </body>
 
-
 {{ HTML::script('bootstrap-multiselect-master/dist/js/bootstrap-multiselect.js') }}
-
-
 {{ HTML::script('js/bl-search.js') }}
-
 
 
 <script type="text/javascript">
