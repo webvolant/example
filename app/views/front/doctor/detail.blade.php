a212deb195ee0f1975bd23f158881765a47c33bd
webvolant <barkalov_anton@mail.ru>
2015-06-16 10:58:38 +0200

    trouble#6

--- app/views/front/doctor/detail.blade.php
+++ app/views/front/doctor/detail.blade.php
@@ -411,7 +411,7 @@ $grafik_arr = explode(";", $user->grafik);
 
 <div class="row otzivi">
                 <?php //var_dump($otzivi->count()); ?>
-                <?php if ($otzivi->count()!=0) { ?>
+                <?php if ($otzivi->count()): ?>
                     <h4 class="h4_my">Отзывы пациентов о враче</h4>
                     <div class="reviews">
                         <?php foreach($otzivi as $item) { ?>
@@ -462,7 +462,7 @@ $grafik_arr = explode(";", $user->grafik);
                     <?php if (($count_otzivi - Helper::reviews_count()) > Helper::reviews_count()) { ?>
                         <a href="" class="more-reviews">Еще {{ $count_otzivi-Helper::reviews_count() }} <?php echo strstr(Lang::choice('mydoc.ot', $count_otzivi, ['n' => $count_otzivi], 'ru'),"о"); ?></a>
                     <?php } ?>
-                <?php } ?>
+                <?php endif; ?>
 </div> <!-- вывод двух отзывов -->
 
 
