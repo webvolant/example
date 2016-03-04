336fd35cd3fb57ab642a0c744e2dce0c0c2fa178
Anton Barkalov <barkalov_anton@mail.ru>
2016-03-04 13:07:53 +0100

    counters_from_mail

--- app/views/front.blade.php
+++ app/views/front.blade.php
@@ -89,6 +89,22 @@
 </head>
 <body>
 
+<!-- Rating@Mail.ru counter -->
+<script type="text/javascript">
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "2751852", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+        if (d.getElementById(id)) return;
+        var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+        ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+        var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+        if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "topmailru-code");
+</script><noscript><div style="position:absolute;left:-10000px;">
+        <img src="//top-fwz1.mail.ru/counter?id=2751852;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />
+    </div></noscript>
+<!-- //Rating@Mail.ru counter -->
+
 <div class="container">
     <div class="row header">
         @section('header')
@@ -590,6 +606,13 @@
                         ga('send', 'pageview');
 
                     </script>
+
+                    <!-- Rating@Mail.ru logo -->
+                    <a href="http://top.mail.ru/jump?from=2751852">
+                        <img src="//top-fwz1.mail.ru/counter?id=2751852;t=479;l=1"
+                             style="border:0;" height="31" width="88" alt="Рейтинг@Mail.ru" /></a>
+                    <!-- //Rating@Mail.ru logo -->
+
 
                 </div>
 
