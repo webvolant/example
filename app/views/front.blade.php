762775cf62fc64fd11ee076aace20408f7712c6c
Anton Barkalov <barkalov_anton@mail.ru>
2016-03-18 07:34:05 +0100

    remarketing

--- app/views/front.blade.php
+++ app/views/front.blade.php
@@ -676,6 +676,26 @@
     </div>
 </noscript>
 </div>
+
+
+<!-- Код тега ремаркетинга Google -->
+<!--------------------------------------------------
+С помощью тега ремаркетинга запрещается собирать информацию, по которой можно идентифицировать личность пользователя. Также запрещается размещать тег на страницах с контентом деликатного характера. Подробнее об этих требованиях и о настройке тега читайте на странице http://google.com/ads/remarketingsetup.
+--------------------------------------------------->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 928239510;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/928239510/?value=0&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
 </body>
 
 {{ HTML::script('bootstrap-multiselect-master/dist/js/bootstrap-multiselect.js') }}
